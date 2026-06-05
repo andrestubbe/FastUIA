429492a36dbd2b4abcb02375a8b6e45ebae9bc21
Andre Stubbe <andrestubbe@example.com>
2026-06-05 05:40:44 +0200

    chore: migrate demos and benchmarks to JitPack

--- run-benchmark.bat
+++ run-benchmark.bat
@@ -1,8 +1,5 @@
-@echo off
-echo ⚡ Building Main Project...
-call mvn clean package -DskipTests
-if %ERRORLEVEL% NEQ 0 ( pause & exit /b )
-echo 🚀 Running Hero Demo...
+﻿@echo off
+echo ðŸš€ Running Hero Demo...
 cd examples\Benchmark
 call mvn compile exec:java -Dexec.mainClass=fastuia.Benchmark
 cd ..\..
--- run-demo.bat
+++ run-demo.bat
@@ -1,8 +1,5 @@
-@echo off
-echo ⚡ Building Main Project...
-call mvn -q clean package -DskipTests
-if %ERRORLEVEL% NEQ 0 ( pause & exit /b )
-echo 🚀 Running Hero Demo...
+﻿@echo off
+echo ðŸš€ Running Hero Demo...
 cd examples\Demo
 call mvn -q compile exec:java -Dexec.mainClass=${exec.mainClass}
 cd ..\..
