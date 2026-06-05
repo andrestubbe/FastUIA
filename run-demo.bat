9f0e286026f9ae17568ea724e7f71656c8be6e88
Andre Stubbe <andrestubbe@proton.me>
2026-06-05 14:00:59 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,6 +1,8 @@
-﻿@echo off
-echo ðŸš€ Running Hero Demo...
+@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
+echo [FastUIA] Running Demo (via JitPack)...
 cd examples\Demo
-call mvn -q compile exec:java -Dexec.mainClass=${exec.mainClass}
+call mvn compile exec:java -Dexec.mainClass=${exec.mainClass}
 cd ..\..
 pause
